9a63fba92f64065bf50a14a0c79bb99352924f26
valegro92 <valegro92@gmail.com>
2025-11-29 15:34:19 +0100

    port

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const tasksRoutes = require('./routes/tasks');
 const extraRoutes = require('./routes/extra');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Security Middleware
